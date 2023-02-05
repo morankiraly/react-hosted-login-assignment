053a53557e151f41b7d5a31d27945c77d1c38a82
morank <morank@rapyd.net>
2023-02-05 13:26:24 +0200

    final chages

---
+++ test_cypress/cypress/e2e/login-test-data.js
@@ -0,0 +1,9 @@
+const test_data = {
+    url: "http://localhost:3000/",
+    loginBaseURL: "https://app-b7nbab8v2i6z.frontegg.com",
+    loginEmail: "morankiraly@gmail.com",
+    loginPassword: "Moranmoran2022!",
+    username: "morank"
+}
+
+export {test_data}
