4d5353e51e22df54ee706a69f77551d43dd84ad0
morank <morank@rapyd.net>
2023-02-05 13:20:44 +0200

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-hosted-login-assignment
-This repository will include a working example of a Cypress project that tests FrontEgg app in a simple login flow of user and password.
--- test_cypress/cypress/e2e/login-test-data.js
+++
@@ -1,9 +0,0 @@
-const test_data = {
-    url: "http://localhost:3000/",
-    loginBaseURL: "https://app-b7nbab8v2i6z.frontegg.com",
-    loginEmail: "morankiraly@gmail.com",
-    loginPassword: "Moranmoran2022!",
-    username: "morank"
-}
-
-export {test_data}
